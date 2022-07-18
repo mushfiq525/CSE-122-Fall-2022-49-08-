aebaec0fb333af95a8bf6233f09e2e3f243a5a9a
Mushfiqur Rahman <109479060+mushfiq525@users.noreply.github.com>
2022-07-18 21:18:50 +0600

    Add files via upload

---
+++ Lab 01/1_ 525_49-08.cpp
@@ -0,0 +1,18 @@
+// codeforces problem no- 4A
+// problem name- watermelon
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    if(n%2==0 && n!=2)
+        cout<<"yes"<<endl;
+    else
+        cout<<"no"<<endl;
+
+    return 0;
+}
+
